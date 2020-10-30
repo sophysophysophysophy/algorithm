10ba3477b57357b1de71c3007f72a981d86d7fbe
정현주 <sophy@hyundai-autoever.com>
2020-10-30 19:04:39 +0900

    07:-4

--- com/exam/IntegerToRoman.java
+++ com/exam/IntegerToRoman.java
@@ -1,4 +1,8 @@
 package exam;
+//https://leetcode.com/problems/integer-to-roman/
+
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class IntegerToRoman {
     public static void main(String[] args) {
@@ -7,6 +11,24 @@ public class IntegerToRoman {
 
 //  greedy
     public static String intToRoman(int num) {
-        return "";
+        String answer = "";
+        String numStr = num + "";
+
+//        Create combination treemap
+        TreeMap<Integer, String> treeMap = new TreeMap<>();
+        treeMap.put(1, "I");
+        treeMap.put(5, "V");
+        treeMap.put(10, "X");
+        treeMap.put(50, "L");
+        treeMap.put(100, "C");
+        treeMap.put(500, "D");
+        treeMap.put(1000, "M");
+
+
+
+//        Get closest number in set ( digits )
+
+
+        return answer;
     }
 }
