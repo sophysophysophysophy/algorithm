3425501d26d47342e7b410e2543fbe1f5116d9da
정현주 <jeonghyeonju@jeonghyeonjuui-MacBookPro.local>
2020-11-03 02:16:12 +0900

    02:16

--- com/exam/MergeIntervals.java
+++ com/exam/MergeIntervals.java
@@ -2,27 +2,31 @@ package exam;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class MergeIntervals {
 
     public static void main(String[] args) {
-        System.out.println(Arrays.deepToString(merge(new int[][]{{1, 3}, {2, 6}, {8, 10}, {15, 19}})));
+//        System.out.println(Arrays.deepToString(merge(new int[][]{{1, 3}, {2, 6}, {8, 10}, {15, 19}})));
+//        System.out.println(Arrays.deepToString(merge(new int[][]{{1, 4}, {4, 5}, {8, 10}, {15, 19}})));
+        System.out.println(Arrays.deepToString(merge(new int[][]{{1, 4}, {0, 1}, {8, 10}, {15, 19}})));
     }
 
     public static int[][] merge(int[][] intervals) {
-        int[][] answer = {} ;
-        List<Integer>[][] myList = new ArrayList[][] {};
-        int idx = 0;
 
+        ArrayList<int[]> list = new ArrayList<>();
 
         for(int i = 0 ; i < intervals.length; i++) {
-//            if(answer.length > 0  && answer[idx][1] >= intervals[i][0]) {
-//
-//            }
+            if(list.size() > 0  && list.get(list.size()-1)[1] >= intervals[i][0]) {
+                int[] removeEle = list.remove(list.size() -1);
+                int firstEle = Math.min(removeEle[0], intervals[i][0]);
+                list.add(new int[] {firstEle,intervals[i][1]});
+            } else list.add (new int[]{intervals[i][0], intervals[i][1]});
         }
 
 
+        int[][] answer = new int[list.size()][2];
+        int idx = 0;
+        for (int[] e: list) answer[idx++] = e;
         return answer;
     }
 }
