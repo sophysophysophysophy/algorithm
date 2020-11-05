b47598089d80029fbdf949fbb589afb6f08879a3
정현주 <jeonghyeonju@jeonghyeonjuui-MacBookPro.local>
2020-11-05 22:25:08 +0900

    10:25

---
+++ com/exam/WordSearch.java
@@ -0,0 +1,22 @@
+package exam;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+//https://leetcode.com/problems/word-search/
+//TODO
+public class WordSearch {
+    public static void main(String[] args) {
+
+    }
+
+//    BFS
+    public static boolean exist(char[][] board, String word) {
+        boolean answer = false;
+
+        Queue<String> queue = new ArrayDeque<>();
+
+
+        return answer;
+    }
+}
