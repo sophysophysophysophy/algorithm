49b644c33bc854f7390a10b21ca197631f9f9081
정현주 <sophy@hyundai-autoever.com>
2020-11-16 16:52:51 +0900

    04;52

--- com/exam/complete/MergeIntervals.java
+++ com/exam/complete/MergeIntervals.java
@@ -1,9 +1,11 @@
-package exam.todo;
+package exam.complete;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 
-//TODO
+//https://leetcode.com/problems/merge-intervals/
+//Complete
 public class MergeIntervals {
 
     public static void main(String[] args) {
@@ -16,11 +18,27 @@ public class MergeIntervals {
 
         ArrayList<int[]> list = new ArrayList<>();
 
+
+//        A  anonymous object (for interface override)
+//        Arrays.sort(intervals, new Comparator<int[]>() {
+//            @Override
+//            public int compare(int[] o1, int[] o2) {
+//                return o1[0] - o2[0];
+//            }
+//        });
+
+//        lambda function (Java8)
+//        Arrays.sort(intervals, (o1, o2) -> o1[0] - o2[0]);
+
+//        Method reference (Java8)
+        Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
+
         for(int i = 0 ; i < intervals.length; i++) {
             if(list.size() > 0  && list.get(list.size()-1)[1] >= intervals[i][0]) {
                 int[] removeEle = list.remove(list.size() -1);
                 int firstEle = Math.min(removeEle[0], intervals[i][0]);
-                list.add(new int[] {firstEle,intervals[i][1]});
+                int secondeEle = Math.max(removeEle[1], intervals[i][1]);
+                list.add(new int[] {firstEle,secondeEle});
             } else list.add (new int[]{intervals[i][0], intervals[i][1]});
         }
 
---
+++ com/exam/todo/WordLadderII.java
@@ -0,0 +1,123 @@
+package exam.todo;
+
+import java.util.*;
+
+//https://leetcode.com/problems/word-ladder-ii/
+//TODO
+public class WordLadderII {
+
+    public static void main(String[] args) {
+        System.out.println(findLadders("hit", "cog", Arrays.asList("hot", "dot", "dog", "lot", "log", "cog")));
+//        System.out.println(findLadders("a","c", Arrays.asList("a","b", "c")));
+    }
+
+    public static List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+        ArrayList<List<String>> answer = new ArrayList<>();
+
+//        Exception Handling
+        if(wordList.size() < 1) return answer;
+        else if(!wordList.contains(endWord)) return answer;
+
+        Queue<StrSet> queue = new ArrayDeque<>();
+        boolean[] flag = new boolean[wordList.size()];
+
+
+
+//        Initialization
+        if (beginWord.equals(endWord)) {
+            answer.add(Arrays.asList(beginWord));
+            return answer;
+        }
+        for(int i = 0 ; i < wordList.size(); i++) {
+            String nowWord = wordList.get(i);
+            if (checkVal(beginWord, nowWord)) {
+                if (nowWord.equals(endWord)) {
+                    return answer;
+                }
+                flag[i] = true;
+                queue.add(new StrSet(nowWord, 2, new ArrayList<>(new )));
+            }
+        }
+
+        boolean minValFlag = false;
+        int minValCnt = 0;
+
+        while (true) {
+            if (queue.size() < 1) {
+                break;
+            }
+
+            StrSet nowSet = queue.poll();
+            String nowWord = nowSet.getWord();
+            for (int i = 0; i < wordList.size(); i++) {
+                String nextWord = wordList.get(i);
+                if(!flag[i] && checkVal(nowWord, nextWord)){
+                    flag[i] = true;
+                    if (nextWord.equals(endWord)) {
+                        if(!minValFlag) {
+                            minValFlag = true;
+                            minValCnt = nowSet.circleVal + 1;
+                        }else if( minValCnt == nowSet.circleVal + 1){
+                            answer.add()
+                        }
+                    }
+                    queue.add(new StrSet(nextWord, nowSet.circleVal + 1));
+                }
+            }
+
+        }
+
+        return answer;
+
+    }
+
+
+    public static boolean checkVal(String beginWord, String nowWord) {
+        char[] beginWordArray = beginWord.toCharArray(), targetWordArray = nowWord.toCharArray();
+
+        int sameCharCnt = 0 ;
+        for (int i = 0; i < beginWordArray.length; i++) {
+            if (beginWordArray[i] != targetWordArray[i]) {
+                sameCharCnt++;
+            }
+        }
+
+        return sameCharCnt == 1;
+    }
+
+    public static class StrSet {
+        private String word;
+        private int circleVal;
+        private List<String> list;
+
+        public StrSet(String word, int circleVal, List<String> list) {
+            this.word = word;
+            this.circleVal = circleVal;
+            this.list = list;
+        }
+
+        public int getCircleVal() {
+            return circleVal;
+        }
+
+        public void setCircleVal(int circleVal) {
+            this.circleVal = circleVal;
+        }
+
+        public String getWord() {
+            return word;
+        }
+
+        public void setWord(String word) {
+            this.word = word;
+        }
+
+        public List<String> getList() {
+            return list;
+        }
+
+        public void setList(List<String> list) {
+            this.list = list;
+        }
+    }
+}
