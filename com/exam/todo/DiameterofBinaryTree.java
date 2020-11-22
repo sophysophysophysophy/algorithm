399661179f6b849bbb993dce72a0f9e631add3bb
정현주 <jeonghyeonju@jeonghyeonjuui-MacBookPro.local>
2020-11-23 00:38:01 +0900

    12:37

--- com/exam/todo/DiameterofBinaryTree.java
+++ com/exam/todo/DiameterofBinaryTree.java
@@ -6,20 +6,31 @@ import exam.TreeNode;
 //https://leetcode.com/problems/diameter-of-binary-tree/
 public class DiameterofBinaryTree {
     public static void main(String[] args) {
+        TreeNode treeNode1 = new TreeNode(4);
+        TreeNode treeNode2 = new TreeNode(5);
+        TreeNode treeNode3 = new TreeNode(2, treeNode1, treeNode2);
+        TreeNode treeNode4 = new TreeNode(3);
+        TreeNode treeNode = new TreeNode(1, treeNode3, treeNode4);
 
+
+
+        System.out.println(diameterOfBinaryTree(treeNode));
     }
 
 
     static int answer;
-    public int diameterOfBinaryTree(TreeNode root) {
+    public static int diameterOfBinaryTree(TreeNode root) {
 
-        answer = 0 ;
-        answer = diameterOfBinaryTree(root, root.left, root.right);
-
+        answer = diameterOfBinaryTreeDFS(root);
         return answer;
     }
 
-//    private int diameterOfBinaryTree(TreeNode root, TreeNode left, TreeNode right) {
-//        return
-//    }
+    private static int diameterOfBinaryTreeDFS(TreeNode root) {
+        if(root.left == null && root.right == null) return 0;
+        int leftVal = root.left == null ? 0 : diameterOfBinaryTree(root.left);
+        int rightVal = root.right == null ? 0 : diameterOfBinaryTree(root.right);
+        answer = Math.max(leftVal + rightVal + 1, answer);
+
+        return Math.max(leftVal + 1, rightVal + 1);
+    }
 }
