67c72d7a9b0ccd3d2a82e5f02d809b4d2617c3a3
정현주 <sophy@hyundai-autoever.com>
2020-12-01 13:01:58 +0900

    01:01

---
+++ com/exam/complete/BalancedBrackets.java
@@ -0,0 +1,24 @@
+package exam.complete;
+//https://www.hackerrank.com/challenges/balanced-brackets/problem
+
+
+import java.util.Stack;
+
+public class BalancedBrackets {
+    static String isBalanced(String s) {
+        String answer = "NO";
+        Stack<Character> stack = new Stack<>();
+
+        char[] array = s.toCharArray();
+        for (char i : array) {
+            if (i == '(' || i == '{' || i == '[') stack.push(i);
+            else {
+                if(stack.empty()) return answer;
+                Character nowChar = stack.pop();
+                if((i == '}' && nowChar == '{') || (i == ']' && nowChar == '[') || (i == ')' && nowChar == '(')) continue;
+                else return answer;
+            }
+        }
+        return stack.empty() ? "YES" : answer;
+    }
+}
---
+++ com/exam/todo/SimpleTextEditor.java
@@ -0,0 +1,48 @@
+package exam.todo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.System.in;
+
+public class SimpleTextEditor {
+
+    public static void main(String[] args) throws IOException {
+        try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in))) {
+            int iterCnt = parseInt(bufferedReader.readLine());
+            System.out.println(iterCnt);
+            StringBuffer nowS = new StringBuffer();
+            List<String> beforeStrList = new ArrayList<>();
+
+            while(iterCnt-- > 0) {
+                StringTokenizer tokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
+                int nowK = parseInt(tokenizer.nextToken());
+                String nowW  = "";
+                if(nowK != 4) nowW = tokenizer.nextToken();
+                switch (nowK) {
+                    case 1 :
+                        if(nowW.length() != 0) beforeStrList.add(nowW);
+                        nowS.append(nowW);
+                        break;
+                    case 2:
+                        nowS.delete(nowS.length() - 1 - (parseInt(nowW) - 1), nowS.length());
+                        if(nowS.length() != 0) beforeStrList.add(String.valueOf(nowS));
+                        break;
+                    case 3 :
+                        System.out.println(nowS.charAt(parseInt(nowW) - 1));
+                        break;
+                    case 4 :
+                        nowS = new StringBuffer();
+                        nowS.append(beforeStrList.get(beforeStrList.size() - 1));
+                        beforeStrList.remove(beforeStrList.size() - 1);
+                        break;
+                }
+            }
+        }
+    }
+}
