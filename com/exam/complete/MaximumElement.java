2f14d708078b0cbde1f3630ab7c883c0ec9829a5
sophysophysophysophy <61459@naver.com>
2020-12-02 18:03:03 +0900

    06:03

---
+++ com/exam/complete/MaximumElement.java
@@ -0,0 +1,43 @@
+package exam.complete;
+//https://www.hackerrank.com/challenges/maximum-element/problem
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.System.in;
+import static java.util.Collections.reverseOrder;
+
+public class MaximumElement {
+    public static void main(String[] args) throws IOException {
+        try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in))) {
+            int iterCnt = parseInt(bufferedReader.readLine());
+            Stack<Integer> stack = new Stack<>();
+            Queue<Integer> queue = new PriorityQueue<>(reverseOrder());
+            while (iterCnt-- > 0) {
+                StringTokenizer tokenizer = new StringTokenizer(bufferedReader.readLine()," ");
+                int nowK = parseInt(tokenizer.nextToken());
+                switch (nowK) {
+                    case 1:
+                        int nowValue = parseInt(tokenizer.nextToken());;
+                        stack.push(nowValue);
+                        queue.add(nowValue);
+                        break;
+                    case 2:
+                        Integer pop = stack.pop();
+                        queue.remove(pop);
+                        break;
+                    default:
+                        System.out.println(queue.peek());
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ com/exam/todo/JesseAndCookies.java
@@ -0,0 +1,35 @@
+package exam.todo;
+//hackerrank.com/challenges/jesse-and-cookies/problem
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Collections.sort;
+
+public class JesseAndCookies {
+
+    static int cookies(int k, int[] A) {
+        List<Integer> list = new ArrayList<>();
+        for (int i : A) list.add(i);
+        int answer = 0;
+        boolean flag = true;
+
+        while (flag) {
+            if(list.size() < 2) return -1;
+            int firstValue = list.remove(0);
+            int secondValue = list.remove(1);
+
+//            Calculating
+            int calVal = firstValue + secondValue * 2;
+            list.add(calVal);
+            sort(list);
+            if(list.get(0) >= k) return answer + 1;
+
+            answer++;
+        }
+
+
+        return answer;
+    }
+}
