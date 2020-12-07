361e0faa52f6d217d630fa4355b3665ce3f31179
sophysophysophysophy <61459@naver.com>
2020-12-07 12:05:29 +0900

    12:05

--- com/exam/todo/CourseScheduleII.java
+++ com/exam/todo/CourseScheduleII.java
@@ -18,30 +18,43 @@ public class CourseScheduleII {
         int[] answer = new int[numCourses - 1];
         if (!canFin) return new int[]{0};
 
-
+        lastAnswerFlag = false;
         for (int i = 0; i < numCourses; i++) {
 //             i == startIdx
             List<Integer> list = new ArrayList<>();
-            makeAnswerByRecur(list, i, courseRelList);
+            list.add(i);
+            makeAnswerByRecur(list, i, courseRelList , 1);
+
+//            list to array (integer object) Using stream && mapToInt && method reference
+            if(lastAnswerFlag) return answerList;
         }
 
 
         return answer;
     }
+    static int[] answerList ;
 
-    private static void makeAnswerByRecur(List<Integer> list, int i, List<List<Integer>> courseRelList) {
+    private static void makeAnswerByRecur(List<Integer> list, int i, List<List<Integer>> courseRelList, int depth) {
+        if(lastAnswerFlag) return;
+        if (depth == list.size()) {
+            answerFlag = true;
+            answerList = list.stream().mapToInt(Integer::intValue).toArray();
+        } else {
+//            DFS
+            for (int j = 0; j < courseRelList.get(i).size(); j++) {
 
+            }
+        }
     }
 
 
+    static boolean lastAnswerFlag ;
     static boolean answerFlag ;
     public static boolean canFinish(int numCourses, int[][] prerequisites, List<List<Integer>> courseRelList) {
         for (int i = 0; i < numCourses; i++) {
             List<Integer> list = new ArrayList<>();
             courseRelList.add(list);
         }
-
-
         for (int[] nowCourse : prerequisites) {
             int postCourse = nowCourse[0];
             int preCourse = nowCourse[1];
