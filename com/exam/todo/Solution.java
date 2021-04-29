af5cee27068c0596a66bddc430e9583e8eebcae2
sophysophysophysophy <61459@naver.com>
2021-04-29 14:18:58 +0900

    02:18

--- com/exam/todo/Solution.java
+++ com/exam/todo/Solution.java
@@ -4,9 +4,20 @@ import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(solution(new int[]{ 2, 1, 3, 4, 1})));
+//        System.out.println(Arrays.toString(solution(new int[]{ 2, 1, 3, 4, 1})));
+        System.out.println(solution(new int[] {1,2,3,4}, new int[] {-3,-1,0,2}));
     }
-    public static int[] solution(int[] numbers) {
+
+    private static int solution(int[] a, int[] b) {
+        int answer = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            answer += a[i] * b[i];
+        }
+        return answer;
+    }
+
+    public static int[] solution1(int[] numbers) {
         List<Integer> list = new ArrayList<>();
 
 //        Arrays.stream(numbers).forEach({e, i} -> {
